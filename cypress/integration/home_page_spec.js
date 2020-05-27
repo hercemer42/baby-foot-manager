0f0ca82430afe4e96aa31f0095f905b5b37aa282
Brian Fox <hercemer42@hotmail.com>
2020-05-27 20:12:46 +0200

    test(e2e): add a couple of basic tests

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -2,4 +2,16 @@ describe('The Home Page', () => {
   it('successfully loads', () => {
     cy.visit('http://localhost:3000') // change URL to match your dev URL
   })
+
+  it ('Renders the title', () => {
+    cy.get('h1').contains('BabyFoot Manager')
+  })
+
+  it ('Creates a game', () => {
+    cy.get('input').first().type('CypressPlayer1')
+    cy.get('input').last().type('CypressPlayer2')
+    cy.get('button').click()
+    cy.wait(100)
+    cy.get('li').first().contains('CypressPlayer1 vs CypressPlayer2')
+  })
 })
